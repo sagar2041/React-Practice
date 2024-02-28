7de21502c6312f3aad0d100fd3d15d2419e77bd9
sagar2041 <patilsagar1722@gmail.com>
2024-02-29 01:10:26 +0530

    changes

--- 3_tailwind-props/src/App.jsx
+++ 3_tailwind-props/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import card from '/componet/card';
+import card from './componet/card'
 
 function App() {
   const [count, setCount] = useState(0)
